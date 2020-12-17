d7829123110d79238856659a9819faa75ff5da90
Ostapenlo Andrei <ostapdans99@gmail.com>
2020-12-18 00:40:03 +0300

    feat: added check for empty string for task1 and task2.

--- index.js
+++ index.js
@@ -1,33 +1,48 @@
 function task1 (){
-    const a = +prompt('Введите число a - число для перевода в новую систему исчисления')
-        if(typeof(a) !== 'number' || a !== a) {
+    const a = prompt('Введите число a - число для перевода в новую систему исчисления')
+        if(typeof(+a) !== 'number' || +a !== +a ) {
             alert('Значение a введено некорректно')
             return task1()
-        } 
-    const b = +prompt('Введите число b - системa исчисления')
-        if(typeof(b) !== 'number' || b !== b) { 
+        } else if(a === '') {
+            alert('Нельзя передавать пустую строку. Введите корректное значение (число)')
+            return task1()
+        }
+    const b = prompt('Введите число b - системa исчисления')
+        if(typeof(+b) !== 'number' || +b !== +b) { 
             alert('Значение b введено некорректно')
+            return task1()
+        } else if(b === '') {
+            alert('Нельзя передавать пустую строку. Введите корректное значение (число)')
+            return task1()
+        } else if(b < 2) {
+            alert('В значение b должно быть передано число больше 2-х')
             return task1()
         }
         alert(a.toString(b))
 }
 
-// task1()
+task1()
 
 
 function task2 () {
-    const a = +prompt('Введите число a')
-        if(typeof(a) !== 'number' || a !== a) {
+    const a = prompt('Введите число a')
+        if(typeof(+a) !== 'number' || +a !== +a ) {
             alert('Значение a введено некорректно')
             return task2()
-    } 
-    const b = +prompt('Введите число b')
-        if(typeof(b) !== 'number' || b !== b) { 
+    } else if(a === '') {
+        alert('Нельзя передавать пустую строку. Введите корректное значение (число)')
+        return task1()
+    }
+    const b = prompt('Введите число b')
+        if(typeof(+b) !== 'number' || +b !== +b) { 
             alert('Значение b введено некорректно')
             return task2()
+        } else if(b === '') {
+            alert('Нельзя передавать пустую строку. Введите корректное значение (число)')
+            return task1()
         }
     const c = prompt('Введите строку')
-        return console.log(`${c}: \n Сумма: ${a - b} \n Частное: ${(a / b).toFixed(2)} `)
+        return console.log(`${c}: \n Разность: ${a - b} \n Частное: ${(a / b).toFixed(2)} `)
 }
 
 // task2()
